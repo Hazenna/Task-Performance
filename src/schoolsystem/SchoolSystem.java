2bdc76a5fba6f99f5099fd36351def14af311f33
Hazenna <Hazenna@MSI>
2025-11-22 22:23:08 +0800

    Task Performance Cleaned Update (Improved some small code)

--- src/schoolsystem/SchoolSystem.java
+++ src/schoolsystem/SchoolSystem.java
@@ -6,7 +6,7 @@ public class SchoolSystem {
 
     public StudentsDB students = new StudentsDB();
     static Scanner s = new Scanner(System.in);
-    private final String admin = "Admin";
+    private String admin = "Admin";
     public static SchoolSystem system = new SchoolSystem();
     public static Teachers teacher = new Teachers();
     public static TeachersDB tdb = new TeachersDB();
@@ -22,6 +22,21 @@ public class SchoolSystem {
                 }
             } catch (NumberFormatException e) {
                 System.out.println("Please Enter a Number between " + min + " and " + max);
+            }
+        }
+    }
+    
+    private void changeCredential() {
+        System.out.print("Enter current credential: ");
+        String input = s.nextLine();
+        if (input.equals(admin)) {
+            System.out.print("Enter new credential: ");
+            String changeCred = s.nextLine();
+            if (changeCred.equals(admin)) {
+                System.out.println("That is already the current credential");
+            }
+            else {
+                admin = changeCred;
             }
         }
     }
@@ -46,7 +61,7 @@ public class SchoolSystem {
                 case 3 -> System.out.println("Feature not implemented yet.");
                 case 4 -> tdb.displayAsAdmin();
                 case 5 -> System.out.println("Feature not implemented yet.");
-                case 6 -> System.out.println("Feature not implemented yet");
+                case 6 -> changeCredential();
             }
         }
     }
@@ -59,8 +74,7 @@ public class SchoolSystem {
             String user = "user";
             choicesMenu(user);
         } else if (cred.equals(this.admin)) {
-            String user = "admin";
-            choicesMenu(user);
+            choicesMenu(admin);
         } else {
             System.out.println("Unknown credential");
             menu();
